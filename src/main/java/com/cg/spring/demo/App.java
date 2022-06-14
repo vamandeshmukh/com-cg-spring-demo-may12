bf8e27977d58da674a3f2320623e3649b1967966
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-14 10:49:27 +0530

    Spring

--- src/main/java/com/cg/spring/demo/App.java
+++ src/main/java/com/cg/spring/demo/App.java
@@ -1,5 +1,11 @@
 package com.cg.spring.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.cg.spring.demo.config.SpringConfig;
+import com.cg.spring.demo.model.Employee;
+
 /**
  * 
  * @author Vaman Deshmukh
@@ -8,10 +14,26 @@ package com.cg.spring.demo;
  *
  */
 
+// Bean == object == instance 
+
+// Spring provides you beans == objects 
+
+// 
+
 public class App {
+
 	public static void main(String[] args) {
 
 		System.out.println("Start");
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+//		Employee emp = new Employee(); // dinner - disposable plates, reusable plates
+
+		Employee emp = context.getBean(Employee.class);
+		
+		emp.setFirstName("Sonu");
+		System.out.println(emp.toString());
 
 		System.out.println("End");
 
