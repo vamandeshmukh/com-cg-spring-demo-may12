24088ed83a5b30b78b3a0b5828d70155047f0717
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-14 14:37:47 +0530

    Spring XML

--- src/main/java/com/cg/spring/demo/App3.java
+++ src/main/java/com/cg/spring/demo/App3.java
@@ -1,8 +1,6 @@
 package com.cg.spring.demo;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
